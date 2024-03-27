6f0618eeab3d584bbe751eca2907baffba0b54f6
PedroMontolive <pedro.montolive@gmail.com>
2024-03-27 17:10:17 -0300

    add: helcome

--- resources/js/Components/HeaderHome.vue
+++ resources/js/Components/HeaderHome.vue
@@ -19,7 +19,7 @@ export default {
     <section id="home">
         <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
             <div class="relative z-20 p-5 text-2xl text-white bg-purple-300 bg-opacity-50 rounded-xl">
-                Welcome to my site!
+                
             </div>
             <video autoplay loop muted class="absolute z-10 w-auto min-w-full min-h-full max-w-none">
                 <source
