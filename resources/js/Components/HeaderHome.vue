563023c3ba3f1d1d80ee78be19851210925bf9c9
PedroMontolive <pedro.montolive@gmail.com>
2024-03-27 17:16:07 -0300

    add: video home

--- resources/js/Components/HeaderHome.vue
+++ resources/js/Components/HeaderHome.vue
@@ -19,11 +19,11 @@ export default {
     <section id="home">
         <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
             <div class="relative z-20 p-5 text-2xl text-white bg-purple-300 bg-opacity-50 rounded-xl">
-                
+
             </div>
             <video autoplay loop muted class="absolute z-10 w-auto min-w-full min-h-full max-w-none">
                 <source
-                    src="https://www.youtube.com/embed/rqJDO3TWnac"
+                    src="video/playback.mp4"
                     type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
